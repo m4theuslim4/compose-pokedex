2ba251f44c414c7d0d817ebc5d0705d96080787d
Matheus Lima <matheuslima9412@gmail.com>
2021-08-22 22:11:20 -0300

    adds colors for pokemon types

--- app/src/main/java/com/theusmalima/pokedex/ui/theme/Color.kt
+++ app/src/main/java/com/theusmalima/pokedex/ui/theme/Color.kt
@@ -6,3 +6,24 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val pokemonColors = mapOf(
+    "grass" to Color(0xFF78C850),
+    "fire" to Color(0xFFF08030),
+    "water" to Color(0xFF6890F0),
+    "bug" to Color(0xFFA8B820),
+    "normal" to Color(0xFFA8A878),
+    "poison" to Color(0xFFA040A0),
+    "electric" to Color(0xFFF8D030),
+    "ground" to Color(0xFFE0C068),
+    "fairy" to Color(0xFFEE99AC),
+    "fighting" to Color(0xFFC03028),
+    "psychic" to Color(0xFFF85888),
+    "rock" to Color(0xFFB8A038),
+    "ghost" to Color(0xFF705898),
+    "ice" to Color(0xFF98D8D8),
+    "dragon" to Color(0xFF7038F8),
+    "flying" to Color(0xFFA890F0),
+    "dark" to Color(0xFF705848),
+    "steel" to Color(0xFFB8B8D0)
+)
